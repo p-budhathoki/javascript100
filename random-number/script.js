4cc2fdb79f60fd1f1553888d7be12ed336da6cca
Patrick <pb760@uowmail.edu.au>
2023-04-05 17:06:05 +1000

    files and folder for random-number updated

---
+++ random-number/script.js
@@ -0,0 +1,11 @@
+const number = document.querySelector(".number");
+const btn = document.querySelector(".generate");
+
+const generateNumber = () => { 
+    // generate a random number between 1 and 10
+    const randomNumber = Math.floor(Math.random() * 10 + 1);
+    number.innerHTML = randomNumber;
+};
+
+// add event listener to the generate button
+btn.addEventListener("click", generateNumber);
