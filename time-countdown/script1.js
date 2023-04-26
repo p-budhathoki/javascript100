13ddc681364f325685e689428d14c16a69c969fa
Patrick <pb760@uowmail.edu.au>
2023-04-26 14:49:49 +1000

    html, css and javascript for time-countdown updated

--- time-countdown/script1.js
+++ time-countdown/script1.js
@@ -1,5 +1,5 @@
-// time countdown
-let time = 1; // time in minutes
+// Time Countdown
+let time = 1; // Time in minutes
 let promoTime = time * 60;
 
 let counting = document.getElementById("countdown");
@@ -8,7 +8,7 @@ function startCountdown() {
   let promoTimer = setInterval(() => {
     if (promoTime <= 0) {
       clearInterval(promoTimer);
-      counting.innerHTML = "Promo has Ended";
+      counting.innerHTML = "Promo has ended.";
     } else {
       promoTime--;
       let day = Math.floor(promoTime / 3600 / 24);
@@ -16,11 +16,15 @@ function startCountdown() {
       let min = Math.floor(promoTime / 60) % 60;
       let sec = Math.floor(promoTime % 60);
 
-      counting.innerHTML = `TIME : ${format(hours)}hr : ${format(min)}min : ${format(sec)}s`;
+      counting.innerHTML = `TIME: ${format(hours)}hr : ${format(
+        min
+      )}min : ${format(sec)}`;
     }
   }, 1000);
 }
+
 function format(t) {
   return t < 10 ? `0${t}` : t;
 }
+
 startCountdown();
