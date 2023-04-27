83b8c93432a20f3b6a9759451594f758c456c64c
Patrick <pb760@uowmail.edu.au>
2023-04-27 15:39:58 +1000

    final updates for page scroll and newsletter

--- page-scroll/script.js
+++ page-scroll/script.js
@@ -4,3 +4,35 @@ window.addEventListener("scroll", () => {
   const header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY);
 });
+
+// scroll progress bar
+window.onscroll = () => scrollProgress();
+
+function scrollProgress() {
+  const currentState =
+    document.body.scrollTop || document.documentElement.scrollTop;
+
+  const pageHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+  const scrollPercentage = (currentState / pageHeight) * 100;
+
+  const progressBar = document.querySelector(".progress");
+  progressBar.style.visibility = "visible";
+  progressBar.style.width = scrollPercentage + "%";
+  
+  // newsletter js
+  const newsLetter = document.querySelector(".newsletter");
+
+if (scrollPercentage > 80) {
+  newsLetter.style.transform = "translateX(0)";
+} else {
+  newsLetter.style.transform = "translateX(-100%)";
+}
+
+document.querySelector(".fa-times").addEventListener("click", () => {
+  newsLetter.style.transform = "translateX(-100%)";
+});
+
+}
