579b1aeebb58e2f8a27932fa98949c537f38d71a
Patrick <pb760@uowmail.edu.au>
2023-04-27 12:21:48 +1000

    html, css and javascript updated for mobile resopnsive

--- responsive-mobile/script.js
+++ responsive-mobile/script.js
@@ -13,4 +13,30 @@ function smoothScroll(e) {
   document.querySelector(href).scrollIntoView({
     behavior: "smooth",
   });
+  hideMenu();
 }
+
+// mobile responsive menu
+// select the navigation links, hamburger menu and close button
+const menu = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+
+const showMenu = () => {
+  // hide hamburger when clicked
+  hamburger.style.display = "none";
+  // show close icon
+  close.style.transform = "translateY(0)";
+  // show menu
+  menu.style.transform = "translateY(0)";
+};
+const hideMenu = () => {
+  // hide close button when clicked
+  close.style.transform = "translateY(-20rem)";
+  hamburger.style.display = "block";
+  // hide menu close icon
+  menu.style.transform = "translateY(-200%)";
+};
+
+hamburger.addEventListener("click", showMenu);
+close.addEventListener("click", hideMenu);
