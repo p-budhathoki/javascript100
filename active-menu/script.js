f8be925579157c9167d0d2d0366abb7fcee5f7b8
Patrick <pb760@uowmail.edu.au>
2023-04-27 10:55:16 +1000

    files and folder for active-menu added

---
+++ active-menu/script.js
@@ -0,0 +1,16 @@
+// select the links
+const links = document.querySelectorAll(".nav-list li a");
+
+for (link of links) {
+  link.addEventListener("click", smoothScroll);
+}
+
+function smoothScroll(e) {
+  e.preventDefault();
+
+  const href = this.getAttribute("href");
+
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+}
