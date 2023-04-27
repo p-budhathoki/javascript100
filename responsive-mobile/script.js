faf0baa518f19a3d4ff8caaab77244aa79de1d6c
Patrick <pb760@uowmail.edu.au>
2023-04-27 14:37:43 +1000

    final updates

--- responsive-mobile/script.js
+++ responsive-mobile/script.js
@@ -18,10 +18,11 @@ function smoothScroll(e) {
 
 // mobile responsive menu
 // select the navigation links, hamburger menu and close button
+const menuWrapper = document.querySelector(".nav-wrapper");
 const menu = document.querySelector(".nav-list");
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close");
-
+/*
 const showMenu = () => {
   // hide hamburger when clicked
   hamburger.style.display = "none";
@@ -37,6 +38,38 @@ const hideMenu = () => {
   // hide menu close icon
   menu.style.transform = "translateY(-200%)";
 };
+*/
+
+const showMenu = () => {
+  hamburger.style.display = "none";
+  close.style.transform = "translateY(0)";
+  menuWrapper.style.transform = "translateX(0)";
+  menu.style.transform = "translateX(0)";
+};
+const hideMenu = () => {
+  close.style.transform = "translateY(-20rem)";
+  hamburger.style.display = "block";
+  menuWrapper.style.transform = "translateX(-200%)";
+  menu.style.transform = "translateX(200%)";
+  submenuThree.style.transform = "translateX(-100%)";
+  
+};
 
 hamburger.addEventListener("click", showMenu);
 close.addEventListener("click", hideMenu);
+menuWrapper.addEventListener("click", hideMenu);
+
+// submenu section
+const thirdLink = document.querySelector(".third-link");
+const back = document.querySelector(".back-to-menu");
+const submenuThree = document.querySelector(".submenu-three");
+
+thirdLink.addEventListener("click", () => {
+  menu.style.transform = "translateX(-100%)";
+  submenuThree.style.transform = "translateX(0)";
+});
+
+back.addEventListener("click", () => {
+  menu.style.transform = "translateX(0)";
+  submenuThree.style.transform = "translateX(-100%)";
+});
