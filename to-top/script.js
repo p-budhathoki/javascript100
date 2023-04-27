ea0896bbb323556d4d27d82de1f04b787e840534
Patrick <pb760@uowmail.edu.au>
2023-04-27 11:56:03 +1000

    html, css and javascript for to-top updated

--- to-top/script.js
+++ to-top/script.js
@@ -19,7 +19,7 @@ function smoothScroll(e) {
 // select the list
 const navList = document.querySelector(".nav-list");
 
-navList.addEventListener("click", (e) => { 
+navList.addEventListener("click", (e) => {
   const navLink = e.target.parentElement;
 
   if (navLink.classList.contains("link")) {
@@ -28,4 +28,29 @@ navList.addEventListener("click", (e) => {
     // add the active class to the current list that is clicked
     navLink.classList.add("active");
   }
- })
+});
+
+// scroll to top
+// get button
+const scrollBtn = document.querySelector(".top");
+const rootEl = document.documentElement;
+
+document.addEventListener("scroll", showBtn);
+scrollBtn.addEventListener("click", scrollToTop);
+
+function showBtn() {
+  const scrollTotal = rootEl.scrollHeight - rootEl.clientHeight;
+
+  if (rootEl.scrollTop / scrollTotal > 0.3) {
+    scrollBtn.classList.add("show-btn");
+  } else {
+    scrollBtn.classList.remove("show-btn");
+  }
+}
+
+function scrollToTop() {
+  rootEl.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
