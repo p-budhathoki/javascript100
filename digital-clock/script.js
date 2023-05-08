fada076fddf51de54dc134e2cc96a0787bfabe67
Patrick <pb760@uowmail.edu.au>
2023-05-08 15:10:22 +1000

    html, css and javascript for digital-clock updated

---
+++ digital-clock/script.js
@@ -0,0 +1,57 @@
+let today = new Date();
+
+function getDate(d) {
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
+
+  return `${day}, ${date}, ${month}, ${year}`;
+}
+
+const date = document.querySelector(".date");
+date.innerHTML = getDate(today);
+
+// get time
+function showTime() {
+  let date = new Date();
+
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+  }
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  // append 0 to single digit
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : s;
+
+  let time = `${h}:${m}:${s} ${session}`;
+  document.querySelector(".time").innerHTML = time;
+}
+
+setInterval(showTime, 1000)
