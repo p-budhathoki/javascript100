b9e00aa747a15eddd8e913f320778585a2012123
Patrick <pb760@uowmail.edu.au>
2023-05-12 12:09:10 +1000

    javascript for module updated

---
+++ module/main.js
@@ -0,0 +1,9 @@
+// import * as varr from "./js/variable.js";
+// import { add } from "./js/function.js";
+import { num1 } from "./js/variable.js";
+
+console.log(varr.num1);
+console.log(varr.num2);
+
+add(varr.num1, varr.num2);
+console.log(add(varr.num1, varr.num2));
