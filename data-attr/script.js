bee2c67526b9810c6540ba04d209f25618ed3768
Patrick <pb760@uowmail.edu.au>
2023-05-17 11:26:32 +1000

    html, css and javascript for data-attr updated

---
+++ data-attr/script.js
@@ -0,0 +1,19 @@
+const btns = document.querySelectorAll(".btn");
+const text = document.querySelector(".text");
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    // const filter = e.target.getAttribute("data-link");
+    // OR
+    const filter = e.target.dataset.link;
+    console.log(filter);
+
+    if (filter == "home") {
+      text.textContent = "Home Page";
+    } else if (filter == "about") {
+      text.textContent = "About Page";
+    } else {
+      text.textContent = "Contact Page";
+    }
+  });
+});
