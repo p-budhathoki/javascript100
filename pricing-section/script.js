c5f961862971a11a06acfc3e838575f4e0c451a0
Patrick <pb760@uowmail.edu.au>
2023-06-11 20:23:57 +1000

    html, css and javascript for pricing section updated

---
+++ pricing-section/script.js
@@ -0,0 +1,22 @@
+// select checkbox, div(slider),span(basic), span(prof) and span(master)
+const checkbox = document.querySelector("#checkbox");
+const slider = document.querySelector(".slider");
+const basic = document.querySelector(".basic");
+const prof = document.querySelector(".prof");
+const master = document.querySelector(".master");
+
+slider.addEventListener("click", () => {
+  if (!checkbox.checked) {
+    [basic.textContent, prof.textContent, master.textContent] = [
+      "120",
+      "240",
+      "360",
+    ];
+  } else {
+    [basic.textContent, prof.textContent, master.textContent] = [
+      "9.99",
+      "19.99",
+      "29.99",
+    ];
+  }
+});
