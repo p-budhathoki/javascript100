a0a4c15cbe3b9c1b48cd8b25a7774c3c743fbafe
Patrick <pb760@uowmail.edu.au>
2023-06-11 12:31:23 +1000

    html, css, script.js and script1.js(added) for multistep-form updated

--- multistep-form/script1.js
+++ multistep-form/script1.js
@@ -1,65 +1,50 @@
-// get form item div, button and paragraph(step)
 const formItems = document.getElementsByClassName("form-item");
 const button = document.getElementsByClassName("btn");
 const steps = document.getElementsByClassName("step");
-// console.log(formItems);
 
 let currentFormItem = 0;
 formItems[currentFormItem].style.display = "block";
 
 if (currentFormItem == 0) {
   button[0].style.display = "none";
-  button[1].style.marginLeft = "0";
   steps[0].style.backgroundColor = "#1e9df7";
 }
 
-// next button
+// Next Button
 button[1].addEventListener("click", () => {
-  // increase the value of current form item by 1
   currentFormItem++;
-  // remember the previous form item using a variable
-  const previousFormItem = currentFormItem - 1;
+  const prevFormItem = currentFormItem - 1;
 
   if (currentFormItem > 0 && currentFormItem < 4) {
     button[0].style.display = "inline-block";
-    //   show next form item
     formItems[currentFormItem].style.display = "block";
-    //   hide the previous form item
-    formItems[previousFormItem].style.display = "none";
-    //   change the background color of steps
+    formItems[prevFormItem].style.display = "none";
     steps[currentFormItem].style.backgroundColor = "#1e9df7";
 
-    //   Change the next button to Submit
     if (currentFormItem == 3) {
       button[1].innerHTML = "Submit";
     }
   } else {
-    //   the next button becomes the submit button. At this point when the submit button is clicked the value of currentFormItem increases by 1
-    //   validate the form
     if (currentFormItem > 3) {
-      alert("Form Submitted Successfully");
+      // Validate the form
+      alert("Form Sunmitted Successfully");
     }
   }
 });
 
-// Previous Button
+// Prev Button
 button[0].addEventListener("click", () => {
   if (currentFormItem > 0) {
     currentFormItem--;
-    // preserve the state of the next form item
     const nextFormItem = currentFormItem + 1;
-    // display the previous form item and hide the current form item
     formItems[currentFormItem].style.display = "block";
     formItems[nextFormItem].style.display = "none";
-    // change the background color of the next form item
-    let stepsColor = rgba(255, 255, 255, 0.3);
     steps[nextFormItem].style.backgroundColor = "#cfd2d4";
 
-    //   hide previous button when the form is at step 1
-    if (currentFormItem === 0) {
+    if (currentFormItem == 0) {
       button[0].style.display = "none";
     }
-    //   if previous button is clicked change the submit button to next button
+
     if (currentFormItem < 3) {
       button[1].innerHTML = "Next";
     }
